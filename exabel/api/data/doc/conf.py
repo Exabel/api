a08b609f5df3eef8d46bfa3e51445187216bd2f5
Børge Nordli <nordli@exabel.com>
2021-06-03 14:39:34 +0200

    Update copyrights

--- exabel/api/data/doc/conf.py
+++ exabel/api/data/doc/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Exabel Data API'
-copyright = '2019-2020, Exabel AS'
+copyright = '2019-2021, Exabel AS'
 author = 'Exabel AS'
 
 # The short X.Y version
